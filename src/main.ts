0cb0e2cd2d3e23adb1df1dc0f497ebb5dc74fb04
puccajp <pisak@jaspal.co.th>
2026-06-17 16:44:39 +0700

    fix: bind application host to 0.0.0.0 for railway network access

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
   
   const port = process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`NestJS Application is running on: http://localhost:${port}`);
   console.log(`Swagger API documentation is available at: http://localhost:${port}/api`);
 }
